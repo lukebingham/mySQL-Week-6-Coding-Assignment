5b3f77de65051e3f2ddedc6b461019ebb1ebf74a
JRNH <jrnh@JRNHs-MacBook-Pro.local>
2020-06-19 16:04:24 -0700

    First commit

--- src/application/App.java
+++ src/application/App.java
@@ -1,9 +1,12 @@
 package application;
 
+import java.sql.SQLException;
+
 public class App {
 
-	public static void main(String[] args) {
-
-	}
-
+	public static void main(String[] args) throws SQLException {
+		Menu menu = new Menu();
+		menu.start();
 }
+}
+
